82992508832e29858d90e9ba3871173de5545664
Hamza Ziyani <hamza.ziyani@koco.online>
2025-05-12 23:25:43 +0100

    Refactor: move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from '@/hooks/useGames';
+import { Game } from "../entities/Game";
 import { HStack, Image } from '@chakra-ui/react';
 import { Card } from '@chakra-ui/react/card';
 import PlatformIconList from './PlatformIconList';

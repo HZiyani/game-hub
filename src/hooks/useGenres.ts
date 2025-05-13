9db5ea7f90cd288266c94561254315fe485cc20f
Hamza Ziyani <hamza.ziyani@koco.online>
2025-05-13 10:40:54 +0100

    Remove initialData property

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "@/services/api-client";
-import genres from "../data/genres";
 import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres")
@@ -9,7 +8,6 @@ const useGenres = () => useQuery({
   queryKey: ["genres"],
   queryFn: apiClient.getAll,
   staleTime: 24 * 60 * 60 * 1000, // 24h
-  initialData: { count: genres.length, results: genres },
 });
 
 export default useGenres;
